8b31729901de0553b8448239d343d6326d79eea8
Msmiles <muhammadirfan123@hotmail.com>
2020-10-02 11:16:27 +0000

    Product admin for adding products updated, inclusive of add product view, url and template

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse,get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -58,3 +60,13 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+    """A view to add a product to the store catalogue"""
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
