6025e5c98da180227f975d680d24a8ef17f57669
Msmiles <muhammadirfan123@hotmail.com>
2020-10-02 14:39:22 +0000

    added edit product function including html, views and urls.py files

--- products/views.py
+++ products/views.py
@@ -80,3 +80,28 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+def edit_product(request, product_id):
+    """A view to edit a product in the store catalogue"""
+    product = get_object_or_404(Product, pk=product_id)
+    
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'the product has successfully updated')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Product update failed. Please ensure the form is completed properly')
+    
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing{product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
