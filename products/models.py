a9e695d59a43c02d613f4f1b0fe65e8ccd2bcfad
Msmiles <muhammadirfan123@hotmail.com>
2020-10-03 07:13:06 +0000

    min and max ValueValidator added to products model for ratings

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Category(models.Model):
 
@@ -22,7 +24,7 @@ class Product(models.Model):
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=3, default=1, validators=[MinValueValidator(1), MaxValueValidator(5)], decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
