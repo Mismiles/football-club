b8556121dfa4f3f7afb85942ac39b31cb04c21ba
Msmiles <muhammadirfan123@hotmail.com>
2020-10-04 09:39:15 +0000

    contact form views, url and forms.py built -html still outstanding

--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,10 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+
+from .views import contactView, successView
+
 
 urlpatterns = [
-    path('', views.contact, name='contact')
+    path('', contactView, name='contact'),
+    path('success/', successView, name='success'),
 ]
