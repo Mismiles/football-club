d391e3fe2cbd3a9605cd42672cb74bb519ef8688
Msmiles <muhammadirfan123@hotmail.com>
2020-10-07 16:59:46 +0000

    attempting to have conact email field appear on email

--- contact/views.py
+++ contact/views.py
@@ -10,7 +10,6 @@ def contactView(request):
     else:
         form = ContactForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
             subject = form.cleaned_data['subject']
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
