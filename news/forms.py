211abf7fc3be2d6f53a6b4c53d3dd43b5a3294f3
Msmiles <muhammadirfan123@hotmail.com>
2020-10-07 08:09:19 +0000

    added django-summernote functionality to news app

---
+++ news/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+
+
+# Apply summernote to specific fields.
+class SomeForm(forms.Form):
+    foo = forms.CharField(widget=SummernoteWidget())  # instead of forms.Textarea
