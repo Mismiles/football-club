bdaa9d772663d2ad696cd7389a970782744027b5
Msmiles <muhammadirfan123@hotmail.com>
2020-10-07 16:24:10 +0000

    attempted to program contact form

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Your full name', required=True)
-    email = forms.EmailField(required=True)
+    from_email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea, required=True, max_length=250)
